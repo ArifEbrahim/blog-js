ead82ba43e316054e739dc9f899d9fa0c5b3308d
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2022-05-01 22:21:56 +0100

    feature: articles can be saved

--- routes/articles.js
+++ routes/articles.js
@@ -1,12 +1,30 @@
 const express = require("express");
+const Article = require("../models/article");
 const router = express.Router();
 
 router.get("/new", (req, res) => {
-  res.render('articles/new');
+  const article = new Article();
+  res.render("articles/new", { article });
 });
 
-router.post('/', (req, res) => {
+router.get('/:id', (req, res) => {
   
 })
+
+router.post("/", async (req, res) => {
+  const { title, description, markdown } = req.body;
+  const article = new Article({
+    title,
+    description,
+    markdown,
+  });
+
+  try {
+    const savedArticle = await article.save();
+    res.redirect(`/articles/${savedArticle.id}`);
+  } catch (e) {
+    res.render("articles/new", { article });
+  }
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -11,15 +11,19 @@ mongoose.connect(process.env.MONGO_URI, {
 
 app.set("view engine", "ejs");
 
-app.use("/articles", articleRouter);
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-  const articles = [{
-    title: 'Test article',
-    createdAt: new Date(),
-    description: 'Test description'
-  }]
-  res.render("articles/index", {articles: articles});
+  const articles = [
+    {
+      title: "Test article",
+      createdAt: new Date(),
+      description: "Test description",
+    },
+  ];
+  res.render("articles/index", { articles: articles });
 });
+
+app.use("/articles", articleRouter);
 
 app.listen(5000);
