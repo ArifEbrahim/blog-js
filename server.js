01d7926e390a367b6561b5a18c15f2ec5e455310
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2022-05-01 17:42:45 +0100

    feature: basic view structure complete

--- server.js
+++ server.js
@@ -7,7 +7,12 @@ app.set("view engine", "ejs");
 app.use("/articles", articleRouter);
 
 app.get("/", (req, res) => {
-  res.render("index");
+  const articles = [{
+    title: 'Test article',
+    createdAt: new Date(),
+    description: 'Test description'
+  }]
+  res.render("index", {articles: articles});
 });
 
 app.listen(5000);
